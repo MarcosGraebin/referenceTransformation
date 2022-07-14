9c66be55b55977c0a3cc20f8700d33fb163026d5
Marcos Graebin <graebin@alunos.utfpr.edu.br>
2022-07-14 13:26:34 -0300

    chore: change numbers in 2 complement

--- main.c
+++ main.c
@@ -2,11 +2,24 @@
 #include <stdlib.h>
 #include "SingleLib.h"
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+inline uint16_t change_to_2_complement(uint16_t number){
+    if(0b1000000000000000 & number){
+        number |= 0b0111111111111111;
+        number = 0 - number;
+    }
+    return number;
 }
 
 vector futureMain(vector point, vector translate, uint16_t rotation){
+}
+
+int main()
+{
+    vector input, translation;
+    input.x = 1;
+    input.y = 2;
+    translation.x = 1;
+    translation.y = 2;
+
+    futureMain(input, translation, 0);
 }
